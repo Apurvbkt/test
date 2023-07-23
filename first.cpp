f5314ea15bbf118aa80ddae89acf1449867afb25
Apurvbkt <91117738+Apurvbkt@users.noreply.github.com>
2023-07-23 20:05:49 +0530

    first commit

---
+++ first.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout << "enter a num : ";
+    cin >> n;
+
+    for(int a = 0; a <= n; a++){
+        cout << "hello world";
+    } 
+}
